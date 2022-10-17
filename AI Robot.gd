88dd94fe370ec3d4d0038e79330cecab1525fcfb
David <mrriptidemc@gmail.com>
2022-10-17 18:44:31 -0400

    actual initial commit

---
+++ AI Robot.gd
@@ -0,0 +1,50 @@
+extends Spatial
+
+
+var target = null
+var rng = RandomNumberGenerator.new()
+onready var nav = $Robot/NavigationAgent
+onready var robot = $Robot
+
+var _velocity = Vector3.ZERO
+
+
+func _ready():
+	rng.randomize()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if target == null:
+		target = get_tree().get_nodes_in_group("targets")[rng.randi_range(0, get_tree().get_nodes_in_group("targets").size() - 1)]
+	nav.set_target_location(target.global_transform.origin)
+	
+	var next_target = nav.get_next_location()
+	print(robot.global_transform.origin.distance_squared_to(next_target))
+	
+	#var _rotation_degrees = robot.rotation_degrees
+	#robot.look_at(next_target, Vector3(0, 1, 0))
+	#var target_degrees = robot.rotation_degrees
+#	robot.rotation_degrees = _rotation_degrees
+#	var rotation_amount = target_degrees.y - _rotation_degrees.y
+	var move_direction = Vector3.ZERO
+#	if abs(rotation_amount) > 0.5:
+#		if rotation_amount > 180:
+#			rotation_amount -= 360
+#		elif rotation_amount < -180:
+#			rotation_amount += 360
+#		robot.rotation_degrees.y += clamp(rotation_amount, -30*delta, 30*delta)
+	if robot.global_transform.origin.distance_squared_to(next_target) > pow((robot._velocity.length_squared())/(2*robot.friction), 2):
+		move_direction.z = -1
+		move_direction = move_direction.rotated(Vector3.UP, robot.rotation.y).normalized()
+#	#robot.move_in_direction(move_direction, delta)
+	
+	if robot.global_transform.origin.distance_squared_to(target.global_transform.origin) < 5:
+		print("found my target")
+		target = null
+	nav.set_velocity(robot.global_transform.origin.direction_to(next_target) * 5)
+
+
+func _on_NavigationAgent_velocity_computed(safe_velocity):
+	#print(safe_velocity)
+	_velocity = robot.move_and_slide_with_snap(safe_velocity, Vector3.DOWN, Vector3.UP, false, 100, 0.785398, false)
---
+++ robot.gd
@@ -0,0 +1,67 @@
+extends KinematicBody
+
+
+export var accel := 5
+export var max_air_speed := 15.0
+export var max_ground_speed := 10.0
+export var jump_strength := 20.0
+export var gravity := 50.0
+export var friction := 0.1
+export var max_extension := 5
+export var extension_speed := 1
+export var rotation_speed := 30
+
+var _extension := 0.0
+var _velocity := Vector3.ZERO
+var _snap_vector := Vector3.DOWN
+
+
+func _ready():
+	$Legs.shape = $Legs.shape.duplicate(true)
+
+
+func move_in_direction(move_direction, delta):
+	var max_speed = max_air_speed
+	if is_on_floor():
+		max_speed = max_ground_speed
+	
+	var accel_adjust = 1
+	if !is_on_floor():
+		accel_adjust = 0.1
+	
+	_velocity.x = lerp(_velocity.x, move_direction.x * max_speed, accel*accel_adjust*delta)
+	_velocity.z = lerp(_velocity.z, move_direction.z * max_speed, accel*accel_adjust*delta)
+	
+	var horiz_vel = Vector2(_velocity.x, _velocity.z)
+	if (horiz_vel.length() != 0):
+		horiz_vel = horiz_vel / horiz_vel.length() * clamp(horiz_vel.length(), -max_speed, max_speed)
+	
+	if is_on_floor():
+		horiz_vel.x = lerp(horiz_vel.x, 0, friction)
+		horiz_vel.y = lerp(horiz_vel.y, 0, friction)
+	
+	_velocity = Vector3(horiz_vel.x, _velocity.y, horiz_vel.y)
+	
+	_velocity.y = clamp(_velocity.y - gravity * delta, -100, 100)
+	
+	var just_landed := is_on_floor() and _snap_vector == Vector3.ZERO
+	# leaving this in just in case i add jumping as a powerup
+	var is_jumping := false and is_on_floor() and Input.is_action_pressed("movement_jump")
+	
+	if is_jumping:
+		_velocity.y = jump_strength
+		_snap_vector = Vector3.ZERO
+	elif just_landed:
+		_snap_vector = Vector3.DOWN
+	_velocity = move_and_slide_with_snap(_velocity, _snap_vector, Vector3.UP, false, 100, 0.785398, false)
+
+
+func extend(amount):
+	_extension = clamp(_extension + amount, 0, max_extension)
+	
+	$Core.translation.y = _extension
+	$Legs.shape.extents.y = _extension
+	$"Legs/Leg Mesh".scale.y = max(2*_extension, 0.001)
+	$Treads.translation.y = -_extension - 1 + 0.1
+	
+	$"Core/Third-Person Camera".scale.z = 1 + _extension * 0.5
